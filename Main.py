a06c33008627644ecc41d2d765773c6982d5404c
Prathamesh <prathamesh.25bai11480@vitbhopal.ac.in>
2025-11-25 00:40:56 +0530

    Made Main file
    made the main file

---
+++ Main.py
@@ -0,0 +1,122 @@
+#service provider class
+
+class Provider:
+
+    def __init__(self, name, rating):
+
+        self.name = name
+
+        self.rating = rating
+
+    #show provider info
+
+    def show(self):
+
+        print(f"Provider: {self.name} | Rating: {self.rating}/5")
+
+#Service class
+
+class Service:
+
+    def __init__(self, name, price, provider):
+
+        self.name = name
+
+        self.price = price
+
+        self.provider = provider
+
+    #SHOW SERVICE WITH PROVIDER
+
+    def show(self):
+
+        print(f"{self.name} - ${self.price}")
+
+        self.provider.show()
+    
+#App class
+
+class HouseServiceApp:
+
+    def __init__(self):
+
+        self.services = [
+
+            Service("Plumber", 50, Provider("Rajat", 4.7)),
+
+            Service("Electrician", 80, Provider("karan", 4.6)),
+
+            Service("Cleaner", 30, Provider("Sarthak", 4.9))
+
+        ]
+
+        self.cart = []
+
+    #Show Service
+
+    def show_services(self):
+
+        for i, s in enumerate(self.services):
+
+            print(f"\n{i+1}. ", end=""); s.show()
+
+    #ADD Service
+
+    def add_to_cart(self, idx):
+
+        self.cart.append(self.services[idx])
+
+        print("Added to cart.")
+
+    # show cart
+
+    def show_cart(self):
+
+        if not self.cart: print("Cart empty"); return
+
+        for s in self.cart: s.show()
+
+    # Checkout
+
+    def checkout(self):
+
+        total = sum(s.price for s in self.cart)
+
+        print(f"Total = ${total}")
+
+# Main
+
+app = HouseServiceApp()
+
+while True:
+
+    print("\n---House Services---")
+
+    print("1. Show Services")
+
+    print("2. Add Service to Cart")
+
+    print("3. View Cart")
+
+    print("4. Checkout")
+
+    print("5. Exit")
+
+    c = input("Choice: ")
+
+    if c == "1": app.show_services()
+
+    elif c == "2":
+
+        app.show_services()
+
+        n=int(input("Select service number")) - 1
+
+        app.add_to_cart(n)
+
+    elif c == "3": app.show_cart()
+
+    elif c == "4": app.checkout()
+
+    elif c == "5": break 
+
